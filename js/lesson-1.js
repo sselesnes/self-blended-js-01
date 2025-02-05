c61fdc01b8bbab93b5c1b1c56820f85adb3ab209
Yuri Boblak <sseslesnes@gmail.com>
2025-02-05 13:51:13 +0200

    arguments .reverse

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -130,14 +130,14 @@ const newSet = new Set(array2);
 console.log((getCommonElements7([1, 5, 2], [2, 1, 17, 3, 19, 5]))); // [1, 2]
 console.table((getCommonElements7([1, 5, 2], [2, 1, 17, 3, 19, 5]))); // [1, 2]
 
-//
-function calculateTotalPrice1(order) {
-    let calc = 0
-    for (const element of order) { calc += element }
-    return calc
-  }
-console.log(calculateTotalPrice1([12, 85, 37, 4]));
-
+/// Шпаргалка по методам масивів:
+// split/join -- перетворює рядок на масив і назад.
+// push () -- додає елементи до кінця,
+// pop() -- видаляє елемент з кінця,
+// shift() -- видаляє елемент із початку,
+// unshift() - додає елементи на початок.
+// splice(index, deleteIndex, ...arr) -- починаючи з індексу index, видаляє deleteIndex елементів і вставляє arr.
+// slice(start, end) -- створює новий масив, копіюючи елементи з позиції start до end (не включаючи end).
 // .map()	Створює новий масив, трансформуючи кожен елемент
 // .filter()	Створює новий масив, відбираючи елементи за умовою
 // .forEach()	Виконує дію для кожного елемента, не повертає значення
@@ -154,4 +154,40 @@ function calculateTotalPrice3(order) {
     return order.reduce(function(accumulator, element) {return accumulator + element});}
 
 console.log(calculateTotalPrice3([412, 371, 94, 63, 176])); // 1116
+
+
+/// Доступ до списку всіх аргументів можна отримати за допомогою спеціальної змінної arguments    
+// 
+function calculateTotalPrice1() {
+    let calc = 0
+    console.log(arguments, typeof (arguments))
+    for (const element of arguments) {
+        console.log(element);
+        calc += element;
+     }
+    return calc }
+console.log(calculateTotalPrice1(1, 2, 3, 4, 5))
+
+//* .reverse повертає масив у зворотному порядку
+// arguments – це псевдомасив, тому його потрібно перетворити на масив.
+//
+function createReversedArray1() {
+    return Array.from(arguments).reverse();
+}
+
+// Щоб додавати нові елементи на початок масиву, зсуваючи попередні вправо, використовуйте метод unshift() 
+//
+function createReversedArray() {
+    let newArray = []
+    for (const element of arguments) {
+        newArray.unshift(element) }
+        return newArray
+    
+ }
+console.log(createReversedArray(412, 371, 94, 63, 176)); // Виведе: [176, 63, 94, 371, 412]
+
+// значення за замовчуванням якщо аргумент не вказано
+function calculateTax(amount, taxRate = 0.2) {
+    return amount * taxRate
+  }
 
