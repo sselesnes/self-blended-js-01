50f624403d5593caab80b0d843fdba7710609736
Yuri Boblak <sseslesnes@gmail.com>
2025-02-13 17:37:35 +0200

    module 5

--- js/module-4.js
+++ js/module-4.js
@@ -483,7 +483,7 @@ alert(`Calories average with empty object: ${calcAverageCalories([])}`)
 //
 alert(`// Object конструктор`)
 //
-// Car - це конструктор
+// Car - це конструктор - визначає функцію-конструктор для створення об'єктів
 function Car(make, model, year) {
   this.make = make;
   this.model = model;
@@ -505,7 +505,7 @@ const carNewParams = {
 }
 // Оператор instanceof перевіряє ланцюг прототипів: Якщо об'єкт є екземпляром конструктора, то в нього є посилання на прототип конструктора в ланцюзі прототипів. Якщо в ланцюгу прототипів знайдеться зазначений конструктор, то перевірка повертає true, в іншому випадку — false.
 
-// Додавання методу до прототипу конструктора Car
+// Додавання методу до прототипу конструктора Car - метод буде доступний лише для екземплярів, створених за допомогою цього конструктора.
 Car.prototype.goodEvening = function(args) {
   console.log(`Доброго вечора ${args}`);
   return;
@@ -576,16 +576,21 @@ console.log(carsSorted4[0].greetings()); // Привіт undefined
 console.log(carsSorted4);
 
 
-// Додавання методу до прототипу конструктора Car
+// Додавання методу до прототипу конструктора Car - метод буде доступний лише для екземплярів, створених за допомогою цього конструктора.
 Car.prototype.goodEvening = function(args) {
   return `Bad ${args}`;
 };
 
 const car1 = new Car("Tesla", "Model S", 2020);
 const car2 = Object.assign(new Car(), car1, carNewParams);
-const car3 = {...car2} // Spread не копіює прототипи об'єкту
+const car3 = {...car2}; // spread (...) створює поверхневу копію об'єкта, копіюючи лише властивості та методи, які безпосередньо належать об'єкту
 
 console.log(car1.greetings(), "world!"); // Good world!
 console.log(car1.goodEvening("time traveler")); // Bad time traveler
 console.log(car2.greetings()); // Ugly car 
-// console.log(car3.goodEvening()); // car3.goodEvening is not a function
+
+console.log(car3.greetings()); // Ugly car - тобто Spread копіює всі властивості та методи, які безпосередньо належать об'єкту car2, але не прототипи
+  
+// console.log(car3.goodEvening()); // error `car3.goodEvening is not a function` - Spread не копіює прототипи об'єкту
+
+// console.log(Car.greetings("world")); // error - метод, доданий до прототипу через Car.prototype.greetings, доступний лише для екземплярів конструктора, а не для самого конструктора.
