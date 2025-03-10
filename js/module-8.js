5406f74ddb4d46994d1d2f2d8ad08be8f2e2c4a8
Yuri Boblak <sseslesnes@gmail.com>
2025-03-10 18:04:36 +0200

    module 8

---
+++ js/module-8.js
@@ -0,0 +1,6 @@
+function parent() {
+  someElement.addEventListener("click", event => {
+    console.log("event.target: ", event.target);
+    console.log("event.currentTarget: ", event.currentTarget);
+  });
+}
