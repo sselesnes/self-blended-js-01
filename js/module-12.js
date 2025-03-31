c26e6e0b48c5b9802528eb9b3b61beb2c9f37d1b
Yuri Boblak <sseslesnes@gmail.com>
2025-03-31 13:11:43 +0300

    async so on

--- js/module-12.js
+++ js/module-12.js
@@ -1,11 +1,39 @@
-// async function fetchData() {
+// асинхронна функція завжди повертає проміс.
+// Якщо функція повертає значення, воно обгортається в проміс.
+// Якщо не повертає значення, створюється проміс із undefined.
+// Асинхронні функції спрощують роботу з промісами, дозволяючи уникнути вкладених .then() і роблячи код читабельнішим.
 
-// const fetchData = async function () {
+function asynсExample1() {
+  // async function fetchData() {
+  const fetchData1 = async function () {
+    return Promise.resolve("someanswer1");
+  };
+  // Тут ми явно створюємо проміс за допомогою Promise.resolve(). Хоча Promise.resolve() створює проміс, який негайно розв'язується, є невелика затримка, пов'язана з внутрішнім механізмом обробки промісів. Ця невелика затримка може бути достатньою, щоб console.log(fetchData1()) виконався до того, як проміс fetchData1() повністю розв'язався. Тому ви бачите Promise {<pending>} при цьому виведення someanswer1 в консоль не означає, що проміс fetchData1() повністю розв'язався на момент виклику console.log(fetchData1()).
 
-// const fetchData = async () => {
+  fetchData1().then(async value => {
+    console.log(await fetchData1()); // someanswer1
+    console.log(value); // someanswer1
+    console.log(fetchData1()); // Promise pending
+  });
 
-// const user = {
-//   async getUsername() {
+  async function main() {
+    console.log(await fetchData1()); // "someanswer1"
+  }
+  main();
 
-// class User {
-//   async getUsername() {
+  const fetchData2 = async () => {
+    return "someanswer2";
+    // У цьому випадку JavaScript автоматично обертає рядок "someanswer2" у проміс, який негайно розв'язується з цим значенням. Тому, коли ви викликаєте fetchData2() у console.log(fetchData2()), проміс вже розв'язаний, і ви бачите Promise {<fulfilled>: 'someanswer2'}.
+  };
+  fetchData2().then(value => {
+    console.log(value); // someanswer2
+    console.log(fetchData2()); // Promise fulfilled
+  });
+
+  // const user = {
+  //   async getUsername() {
+
+  // class User {
+  //   async getUsername() {
+}
+asynсExample1();
