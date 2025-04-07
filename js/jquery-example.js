a3788ea16572b196620e78d8b6514782f59421ca
Yuri Boblak <sseslesnes@gmail.com>
2025-04-07 17:54:57 +0300

    gsap

--- js/jquery-example.js
+++ js/jquery-example.js
@@ -10,8 +10,17 @@ $(document).ready(function () {
     }, 1000);
 
     counter++;
-    $createBtn.after(`<p class="counter-text">pressed ${counter}</p>`);
+
+    // $createBtn.after(`<p class="counter-text">pressed ${counter}</p>`);
     // $("body").append(`<p class="counter-text">pressed ${counter}</p>`);
+
+    const $newText = $(`<p class="counter-text">pressed ${counter}</p>`).hide();
+    $createBtn.after($newText);
+    gsap.fromTo(
+      $newText,
+      { autoAlpha: 0, display: "none" }, // Initial state
+      { autoAlpha: 1, display: "block", duration: 1.3 } // Final state
+    );
   });
 
   $destroyBtn.on("click", function () {
